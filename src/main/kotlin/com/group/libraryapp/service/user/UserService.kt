35ced382c111ddb2d461da0b93f4fa1eadcff5d1
xeroman.kyn <xeroman.k@gmail.com>
2023-04-05 11:10:24 +0900

    옵셔널 제거

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -6,6 +6,9 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
+import com.group.libraryapp.util.fail
+import com.group.libraryapp.util.findByIdOrThrow
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -28,15 +31,13 @@ class UserService(
 
     @Transactional
     fun updateUserName(request: UserUpdateRequest){
-        val user = userRepository.findById(request.id)
-            .orElseThrow(::IllegalArgumentException)
+        val user = userRepository.findByIdOrThrow(request.id)
         user.updateName(request.name)
     }
 
     @Transactional
     fun deleteUser(name: String){
-        val user = userRepository.findByName(name)
-            .orElseThrow(::IllegalArgumentException)
+        val user = userRepository.findByName(name) ?: fail()
         userRepository.delete(user)
     }
 }
