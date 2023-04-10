6bd8bc8453be34c614758f5fa9a65ec6a4906f28
xeroman.kyn <xeroman.k@gmail.com>
2023-04-10 11:33:02 +0900

    별도의 클래스 작성을 통한 querydsl 작성

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.Query
 interface BookRepository : JpaRepository<Book, Long> {
     fun findByName(bookName: String): Book?
 
-    @Query("" +
-        " select new com.group.libraryapp.dto.book.response.BookStatResponse(b.type, count(b.id))" +
-        "   from Book b" +
-        "  group by b.type"
-    )
-    fun getStats(): List<BookStatResponse>
+//    @Query("" +
+//        " select new com.group.libraryapp.dto.book.response.BookStatResponse(b.type, count(b.id))" +
+//        "   from Book b" +
+//        "  group by b.type"
+//    )
+//    fun getStats(): List<BookStatResponse>
 }
--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -9,6 +9,7 @@ import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.repository.book.BookQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional
 class BookService (
     private val bookRepository: BookRepository,
     private val userRepository: UserRepository,
+    private val bookQuerydslRepository: BookQuerydslRepository,
     private val userLoanHistoryRepository: UserLoanHistoryRepository
     ){
 
@@ -53,7 +55,8 @@ class BookService (
     @Transactional(readOnly = true)
     fun getBookStatistics(): List<BookStatResponse> {
 
-        return bookRepository.getStats()
+        return bookQuerydslRepository.getStats()
+//        return bookRepository.getStats()
 //        return bookRepository.findAll()
 //            .groupBy { book -> book.type }
 //            .map { (type, books) -> BookStatResponse(type, books.size) }
